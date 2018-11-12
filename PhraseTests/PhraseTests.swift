f61fde969184953dc4ac435939b1e60ca78b332b
Valentin Knabel <dev@vknabel.com>
2018-11-12 22:20:40 +0100

    Initial Commit

---
+++ PhraseTests/PhraseTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhraseTests.swift
+//  PhraseTests
+//
+//  Created by Valentin Knabel on 12.11.18.
+//  Copyright © 2018 Valentin Knabel. All rights reserved.
+//
+
+import XCTest
+@testable import Phrase
+
+class PhraseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
